f3917cba6933b06a112557b4d4f42fb8ddf1db8f
BuildTools <unconfigured@null.spigotmc.org>
2022-12-26 16:28:01 +0600

    add routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Users from "./components/users";
+import NavBar from "./components/navBar/navBar";
+import Main from "./components/main";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/login";
+import User from "./components/user";
 
 function App() {
-    return <Users />;
+    return (
+        <div>
+            <NavBar />
+            <Switch>
+                <Route path="/" exact component={Main} />
+                <Route
+                    path="/users/:userId"
+                    render={(props) => <User {...props} />}
+                />
+                <Route path="/login" component={Login} />
+                <Route path="/users" component={Users} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
