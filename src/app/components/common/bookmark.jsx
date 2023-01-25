0eb516851fd824becfe6007fc3ed3cdc0fdaaf58
Vladyslav <vlad.ur.ua04@gmail.com>
2023-01-25 15:01:25 +0600

    add common folder

---
+++ src/app/components/common/bookmark.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Bookmark = ({ status, ...rest }) => {
+    return (
+        <button {...rest}>
+            <i className={"bi bi-bookmark" + (status ? "-fill" : "")}></i>
+        </button>
+    );
+};
+
+Bookmark.propTypes = {
+    status: PropTypes.bool.isRequired
+};
+export default Bookmark;
