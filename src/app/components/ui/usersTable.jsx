4c8c0c5e73b1933ace6345b84794501190704230
Vladyslav <vlad.ur.ua04@gmail.com>
2023-01-25 15:03:22 +0600

    add ui folder

---
+++ src/app/components/ui/usersTable.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+import BookMark from "../common/bookmark";
+import Qualities from "./qualities";
+import Table from "../common/table";
+
+const UserTable = ({
+    users,
+    onSort,
+    selectedSort,
+    onToggleBookMark,
+    onDelete
+}) => {
+    const columns = {
+        name: {
+            path: "name",
+            name: "Имя ",
+            sortStatus: "иконка"
+        },
+        qualities: {
+            name: "Качества",
+            component: (user) => <Qualities qualities={user.qualities} />
+        },
+        professions: { path: "profession.name", name: "Профессия" },
+        completedMeetings: {
+            path: "completedMeetings",
+            name: "Встретился, раз"
+        },
+        rate: { path: "rate", name: "Оценка" },
+        bookmark: {
+            path: "bookmark",
+            name: "Избранное",
+            component: (user) => (
+                <BookMark
+                    status={user.bookmark}
+                    onClick={() => onToggleBookMark(user._id)}
+                />
+            )
+        },
+        delete: {
+            component: (user) => (
+                <button
+                    onClick={() => onDelete(user._id)}
+                    className="btn btn-danger"
+                >
+                    Удалить
+                </button>
+            )
+        }
+    };
+    return (
+        <Table
+            onSort={onSort}
+            selectedSort={selectedSort}
+            columns={columns}
+            data={users}
+        />
+    );
+};
+
+UserTable.propTypes = {
+    users: PropTypes.array.isRequired,
+    onSort: PropTypes.func.isRequired,
+    selectedSort: PropTypes.object.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
+};
+
+export default UserTable;
