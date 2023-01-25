021345ab26583b0f46a6acab6dabd7918331986c
Vladyslav <vlad.ur.ua04@gmail.com>
2023-01-25 15:07:21 +0600

    add validator

--- src/app/utils/validator.js
+++ src/app/utils/validator.js
@@ -3,20 +3,25 @@ export function validator(data, config) {
     function validate(validateMethod, data, config) {
         let statusValidate;
         switch (validateMethod) {
-            case "isRequired":
-                statusValidate = data.trim() === "";
+            case "isRequired": {
+                if (typeof data === "boolean") {
+                    statusValidate = !data;
+                } else {
+                    statusValidate = data.trim() === "";
+                }
                 break;
+            }
             case "isEmail": {
                 const emailRegExp = /^\S+@\S+\.\S+$/g;
                 statusValidate = !emailRegExp.test(data);
                 break;
             }
-            case "isCapitalSymbol": {
+            case "isCapitalSybmol": {
                 const capitalRegExp = /[A-Z]+/g;
                 statusValidate = !capitalRegExp.test(data);
                 break;
             }
-            case "isContainDigital": {
+            case "isContainDigit": {
                 const digitRegExp = /\d+/g;
                 statusValidate = !digitRegExp.test(data);
                 break;
@@ -25,12 +30,12 @@ export function validator(data, config) {
                 statusValidate = data.length < config.value;
                 break;
             }
+
             default:
                 break;
         }
         if (statusValidate) return config.message;
     }
-
     for (const fieldName in data) {
         for (const validateMethod in config[fieldName]) {
             const error = validate(
