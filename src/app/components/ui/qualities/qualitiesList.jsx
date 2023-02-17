e0507662d6fbb5c8afa04281627f3651d4b4ab83
Vladyslav <vlad.ur.ua04@gmail.com>
2023-02-17 20:58:25 +0600

    refactoring

--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Qualitie from "./qualitie";
+import { useQualities } from "../../../hooks/useQualities";
 
 const QualitiesList = ({ qualities }) => {
+    const { isLoading, getQuality } = useQualities();
+
+    if (isLoading) {
+        return isLoading;
+    }
+
     return (
         <>
-            {qualities.map((qual) => (
-                <Qualitie {...qual} key={qual._id} />
+            {qualities.map((id) => (
+                <Qualitie {...getQuality(id)} key={id} />
             ))}
         </>
     );
