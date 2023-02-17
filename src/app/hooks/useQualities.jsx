fd394637245326380bde3bac5d67c36cc58f785e
Vladyslav <vlad.ur.ua04@gmail.com>
2023-02-17 20:49:03 +0600

    add hooks folder

---
+++ src/app/hooks/useQualities.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import qualitiesService from "../services/qualities.service";
+import { toast } from "react-toastify";
+
+const QualitiesContext = React.createContext();
+
+export const useQualities = () => {
+    return useContext(QualitiesContext);
+};
+
+export const QualitiesProvider = ({ children }) => {
+    const [qualities, setQualities] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getQualitiesList();
+    }, []);
+
+    useEffect(() => {
+        if (error !== null) {
+            toast.error(error);
+            setError(null);
+        }
+    }, [error]);
+
+    async function getQualitiesList() {
+        try {
+            const { content } = await qualitiesService.get();
+            setQualities(content);
+            setLoading(false);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+
+    const getQuality = (id) => {
+        return qualities.find((q) => q._id === id);
+    };
+
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+
+    return (
+        <QualitiesContext.Provider value={{ isLoading, qualities, getQuality }}>
+            {children}
+        </QualitiesContext.Provider>
+    );
+};
+
+QualitiesProvider.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes
+    ])
+};
