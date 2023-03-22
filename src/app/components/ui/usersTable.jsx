84f5065ef323d04093d0c96418ae82cd8f3fd783
Vladyslav <vlad.ur.ua04@gmail.com>
2023-03-22 13:17:03 -0300

    some changes

--- src/app/components/ui/usersTable.jsx
+++ src/app/components/ui/usersTable.jsx
@@ -5,13 +5,7 @@ import Qualities from "./qualities";
 import Table from "../common/table";
 import Profession from "./professions";
 
-const UserTable = ({
-    users,
-    onSort,
-    selectedSort,
-    onToggleBookMark,
-    onDelete
-}) => {
+const UserTable = ({ users, onSort, selectedSort, onToggleBookMark }) => {
     const columns = {
         name: {
             path: "name",
@@ -40,16 +34,6 @@ const UserTable = ({
                     onClick={() => onToggleBookMark(user._id)}
                 />
             )
-        },
-        delete: {
-            component: (user) => (
-                <button
-                    onClick={() => onDelete(user._id)}
-                    className="btn btn-danger"
-                >
-                    Удалить
-                </button>
-            )
         }
     };
     return (
@@ -66,8 +50,7 @@ UserTable.propTypes = {
     users: PropTypes.array.isRequired,
     onSort: PropTypes.func.isRequired,
     selectedSort: PropTypes.object.isRequired,
-    onToggleBookMark: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired
+    onToggleBookMark: PropTypes.func.isRequired
 };
 
 export default UserTable;
