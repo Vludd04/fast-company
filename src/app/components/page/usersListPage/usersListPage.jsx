a6737aaa3bdaa348e1cf3117dba4ec2cfd623b19
Vladyslav <vlad.ur.ua04@gmail.com>
2023-04-10 20:27:24 -0300

    updated user function

--- src/app/components/page/usersListPage/usersListPage.jsx
+++ src/app/components/page/usersListPage/usersListPage.jsx
@@ -7,13 +7,20 @@ import SearchStatus from "../../ui/searchStatus";
 import UserTable from "../../ui/usersTable";
 import { useUser } from "../../../hooks/useUsers";
 import _ from "lodash";
-import { useProfession } from "../../../hooks/useProfession";
 import { useAuth } from "../../../hooks/useAuth";
+import { useSelector } from "react-redux";
+import {
+    getProfessions,
+    getProfessionsLoadingStatus
+} from "../../../store/professions";
 
 const UsersListPage = () => {
     const { users } = useUser();
     const { currentUser } = useAuth();
-    const { isLoading: professionsLoading, professions } = useProfession();
+    const professions = useSelector(getProfessions());
+    const { isLoading: professionsLoading } = useSelector(
+        getProfessionsLoadingStatus()
+    );
     const [currentPage, setCurrentPage] = useState(1);
     const [searchQuery, setSearchQuery] = useState("");
     const [selectedProf, setSelectedProf] = useState();
