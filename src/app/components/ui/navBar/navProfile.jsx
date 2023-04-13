d954b28fd09435a614cd4cee51ee6e4026528656
Vladyslav <vlad.ur.ua04@gmail.com>
2023-04-13 18:26:25 -0300

    removed useAuth

--- src/app/components/ui/navBar/navProfile.jsx
+++ src/app/components/ui/navBar/navProfile.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { useAuth } from "../../../hooks/useAuth";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getCurrentUserData } from "../../../store/users";
 
 const NavProfile = () => {
-    const { currentUser } = useAuth();
+    const currentUser = useSelector(getCurrentUserData());
     const [isOpen, setOpen] = useState(false);
     const toggleMenu = () => {
         setOpen((prevState) => !prevState);
     };
+    if (!currentUser) return "loading";
 
     return (
         <div className="dropdown" onClick={toggleMenu}>
