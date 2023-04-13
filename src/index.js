fde21e1cace6f9a0717530fd1f797758f166329b
Vladyslav <vlad.ur.ua04@gmail.com>
2023-04-13 18:33:00 -0300

    added history

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { createStore } from "./app/store/createStore";
 import { Provider } from "react-redux";
+import history from "./app/utils/history";
 
 const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <Router history={history}>
         <Provider store={store}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
         </Provider>
-    </BrowserRouter>
+    </Router>
 );
